f337df1f43bf15bd59ec3f15f9fddb4b99469bfa
dmitry kolobov <dmkolobov@gmail.com>
2019-03-17 15:34:35 -0600

    Add documentation and rename type.

--- src/Text/Doc.purs
+++ src/Text/Doc.purs
@@ -12,13 +12,15 @@ import Data.Tuple
 
 import Data.Sequence
 
-data DocF = Inline Int (Seq String) 
-         | Block Int DocF (Seq DocF)  
+data Doc = Inline Int (Seq String) 
+         | Block Int Doc (Seq Doc)  
 
-inline :: String -> DocF 
+-- | Returns an inline document containing the given string. 
+inline :: String -> Doc 
 inline = Inline 0 <<< singleton 
 
-block :: Seq DocF -> DocF 
+-- | Returns a block document containing the given documents. 
+block :: Seq Doc -> Doc 
 block ds = case uncons ds of 
              Just (Tuple head tail) -> Block 0 head tail 
              Nothing                -> Inline 0 mempty
@@ -29,62 +31,80 @@ ws n = S.fromCodePointArray (replicate n $ codePointFromChar ' ')
 
 -- | Takes an accumulator containing the identation and a doc, and returns 
 -- | a string representing that doc.  
-print :: Int -> DocF -> String 
+print :: Int -> Doc -> String 
 print tab (Inline pad ss)  = ws (tab + pad) <> intercalate " " ss 
 print tab (Block pad d ds) = intercalate "\n"
                            $ (print $ tab + pad) <$> (cons d ds)
 
-inlined :: DocF -> String 
+-- | Show the enire document as a single line and return that string 
+-- | representation.
+inlined :: Doc -> String 
 inlined (Inline _ ss)  = intercalate " " ss
 inlined (Block _ d ds) = S.joinWith " " <<< toUnfoldable $ inlined <$> (cons d ds)
 
-instance showDocF :: Show DocF 
+instance showDoc :: Show Doc 
   where 
   show = print 0
 
-indent :: Int -> DocF -> DocF 
+-- | Indent the entire doc `n` spaces. 
+indent :: Int -> Doc -> Doc 
 indent tab (Inline pad s)   = Inline (tab + pad) s 
 indent tab (Block pad d ds) = Block (tab + pad) d ds 
 
-prepend :: String -> DocF -> DocF
-prepend pre (Inline pad ss)  = Inline 0 (cons pre ss)
-prepend pre (Block pad d ds) = block <<< cons (prepend pre d) 
-                                  $ indent (S.length pre + 1) <$> ds
+-- | Places the given string to the left of the given doc, shifting the rest of the 
+-- | doc so that the indentation is preserved. 
+beside :: String -> Doc -> Doc
+beside pre (Inline pad ss)  = Inline 0 (cons pre ss)
+beside pre (Block pad d ds) = block <<< cons (beside pre d) 
+                            $ indent (S.length pre + 1) <$> ds
 
-prefix :: String -> DocF -> DocF 
+-- | Prepends a string to the first line in the document and shifts lines in the 
+-- | same block as that line to preserve indentation. 
+prefix :: String -> Doc -> Doc 
 prefix pre (Inline pad ss)  = Inline 0 (cons pre ss) 
 prefix pre (Block pad d ds) = block $ cons (indentBlock (S.length pre + 1) (prefix pre d)) ds
 
-flow :: DocF -> DocF -> DocF 
-flow line@(Inline pad ss) docR = indent pad $ (prepend (inlined line) docR)
+-- | Places the second document after the last line of the first document. 
+-- | The second document is shifted so that indentation is preserved. 
+flow :: Doc -> Doc -> Doc 
+flow line@(Inline pad ss) docR = indent pad $ (beside (inlined line) docR)
 flow (Block pad d ds) docR 
   = case unsnoc ds of 
       Just (Tuple init last) -> indent pad <<< block <<< cons d <<< snoc init
                               $ flow last docR 
       Nothing -> block <<< singleton $ flow d docR
 
-infixr 4 flow as :->
+infixr 4 flow as :>
 
-indentBlock :: Int -> DocF -> DocF 
+-- | Indent all but the first line of the given document by `n` spaces.
+indentBlock :: Int -> Doc -> Doc 
 indentBlock n (Inline pad ss)  = Inline pad ss 
 indentBlock n (Block pad d ds) = Block pad d (indent n <$> ds) 
 
-between :: String -> String -> DocF -> DocF 
-between open close doc = prefix open doc :-> inline close
+-- | Returns the given doc with surrounded with the given `open` and 
+-- | `close` strings. 
+between :: String -> String -> Doc -> Doc 
+between open close doc = prefix open doc :> inline close
 
-parens :: DocF -> DocF 
+-- | Returns the given doc surrounded with parenthesis. 
+parens :: Doc -> Doc 
 parens = between "(" ")"
 
-brackets :: DocF -> DocF 
+-- | Returns the given doc surrounded with square brackets.
+brackets :: Doc -> Doc 
 brackets = between "[" "]"
 
-braces :: DocF -> DocF 
+-- | Returns the given doc surrounded with curly braces. 
+braces :: Doc -> Doc 
 braces = between "{" "}"
 
-isEmpty :: DocF -> Boolean 
+-- | Returns true when given the empty doc. 
+isEmpty :: Doc -> Boolean 
 isEmpty (Inline _ ss) = null ss 
 isEmpty _             = false
 
-intermix :: String -> DocF -> DocF 
+-- | Place the given string beside every line in the given doc but the 
+-- | first. 
+intermix :: String -> Doc -> Doc 
 intermix sep line@(Inline _ _) = line
-intermix sep (Block pad d ds)  = Block pad d $ prepend sep <$> ds
+intermix sep (Block pad d ds)  = Block pad d $ beside sep <$> ds
